c3896dcb6b4b707189c743f8ddb367e40c6d3f55
Krishankant89 <toxickush90@gmail.com>
2026-04-30 23:34:41 +0530

    Two pointer merger in array

---
+++ Merging/twopointerarray.py
@@ -0,0 +1,28 @@
+n1 = int(input("Enter size of first sorted array: "))
+arr1 = [int(input()) for _ in range(n1)]
+
+n2 = int(input("Enter size of second sorted array: "))
+arr2 = [int(input()) for _ in range(n2)]
+
+merged = []
+i = j = 0
+
+# Merge while both arrays have elements
+while i < n1 and j < n2:
+    if arr1[i] < arr2[j]:
+        merged.append(arr1[i])
+        i += 1
+    else:
+        merged.append(arr2[j])
+        j += 1
+
+# Remaining elements
+while i < n1:
+    merged.append(arr1[i])
+    i += 1
+
+while j < n2:
+    merged.append(arr2[j])
+    j += 1
+
+print("Merged sorted array:", merged)
